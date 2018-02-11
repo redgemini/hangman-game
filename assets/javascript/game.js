00a4c05ec0270168304754a6612883653b736171
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-11 17:40:31 -0600

    fileSetUp

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Pseudocode
+//generate a word at random
+//correct guesses is less than length of word
+//promt user to guess a letter
+//if guess correct increment correctGuess by one
+//if guess incorrect increment incorrectGuess by one
+//if incorrect guesses greater than 5
+
