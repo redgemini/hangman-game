232395b4b35fef132313f0877ffcd2f1c02af2d5
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-11 22:59:10 -0600

    clearAsMud

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,40 @@
 //Pseudocode
-//generate a word at random
-//correct guesses is less than length of word
-//promt user to guess a letter
+//start=generate a word at random
+//correct guesses is less than length of word =Spaces and word letters
+//promt user to guess a letter =
 //if guess correct increment correctGuess by one
 //if guess incorrect increment incorrectGuess by one
 //if incorrect guesses greater than 5
 
-//Global Variables
-//========================================================================================================
 
 //Arrays and Variables
 var wordOptions = ["bob schneider", "janis joplin", "robert earl keen", "townes van zandt", 
-    "stevie ray vaughn", "willie nelson", "gary clark jr", "jimmie vaughan", "monte montgomery"]
+    "stevie ray vaughn", "willie nelson", "gary clark jr", "jimmie vaughan", "monte montgomery"];
 var selectedWord = "";
-var lettersinWord = []
-var numBlanks = 0;
-var blanksAndSuccesses = [];
+var lettersInWord = []
+var numSpaces = 0;
+var spacesAndSuccesses = [];
 var wrongLetters = [];
 
-//Game Counters
+//Game Stats
 
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 9;
+var totalWins = 0;
+var totalLosses = 0;
+var guessesRemaining= 9;
 
 //Functions
-//=================================
+function startGame() {
+selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    console.log(selectedWord);
+lettersInWord = selectedWord.split("");
+    console.log(lettersInWord);
+numSpaces = lettersInWord.length;
+    console.log(numSpaces);
 
+for(var i=0; i<numSpaces; i++){
+
+}
+}
 //Main Process 
-//==================================
+
+startGame();
