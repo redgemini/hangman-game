abfe744b6fbfb84d40209f84902723c42c2fb448
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-11 21:58:31 -0600

    add-variables_arrays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,5 +6,26 @@
 //if guess incorrect increment incorrectGuess by one
 //if incorrect guesses greater than 5
 
-alert("test");
-alert("HELP");
+//Global Variables
+//========================================================================================================
+
+//Arrays and Variables
+var wordOptions = ["bob schneider", "janis joplin", "robert earl keen", "townes van zandt", 
+    "stevie ray vaughn", "willie nelson", "gary clark jr", "jimmie vaughan", "monte montgomery"]
+var selectedWord = "";
+var lettersinWord = []
+var numBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters = [];
+
+//Game Counters
+
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 9;
+
+//Functions
+//=================================
+
+//Main Process 
+//==================================
